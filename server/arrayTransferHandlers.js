10fa26ab06610933f1879967e5458fa1ae38a221
dnaganog <dnaganog@gmail.com>
2019-07-31 11:45:05 -0400

    updated server-side handlers for arrayTransfer

--- server/arrayTransferHandlers.js
+++ server/arrayTransferHandlers.js
@@ -1,11 +1,25 @@
 ClientToServerHandler( onClientStream ) {
-  onClientStream.send({ 
-    response: value 
-  });
+  let resArray = [];
+  onClientStream.on('data', (array) => {
+    resArray = resArray.concat(array);
+  })
+  onClientStream.on('end', () => {
+    onClientStream.send({ 
+      status: true,
+      comments: 'response from server: the client stream is complete',
+      array : resArray,
+    });
+  })
 };
-ServerToClientHandler( CALL ) {
-  CALL.on('data', request => someFunctionality(request));
-  CALL.send({ response: value });
+ServerToClientHandler( onClientCall ) {
+  let count = 0;
+  const timer = setInterval(() => {
+    count += 1;
+    onClientCall.write({array: [count, count / 2]})
+  }, 1);
+  setTimeout(() => {
+    clearInterval(timer);
+  }, 5000);
 };
 module.exports = { 
 	ClientToServerHandler,
--- server/server.js
+++ server/server.js
@@ -2,18 +2,18 @@ const { Server } = require( 'firecomm' );
 const package = require( '../package.js' );
 const { ClientToServerHandler,
 	ServerToClientHandler } = require( './arrayTransferHandlers.js' );
-const { UnaryMathHandler,
-	BidiMathHandler } = require( './heavyMathHandlers.js' );
+// const { UnaryMathHandler,
+// 	BidiMathHandler } = require( './heavyMathHandlers.js' );
 
 const server = new Server();
 server.addService( package.ArrayTransfer,   { 
   ClientToServer: ClientToServerHandler,
   ServerToClient: ServerToClientHandler,
 });
-server.addService( package.HeavyMath,   { 
-  UnaryMath: UnaryMathHandler,
-  BidiMath: BidiMathHandler,
-});
+// server.addService( package.HeavyMath,   { 
+//   UnaryMath: UnaryMathHandler,
+//   BidiMath: BidiMathHandler,
+// });
 server.bind( [ 
   '0.0.0.0: 3000', 
   '0.0.0.0: 2999', 
