310ed0d3e4dabcd7fd972fcb21f6f3eb4f59d4ac
dnaganog <dnaganog@gmail.com>
2019-07-31 11:02:33 -0400

    added config object to package.js

--- package.js
+++ package.js
@@ -2,6 +2,10 @@ const { build } = require('firecomm');
 const path = require('path');
 
 const PROTO_PATH = path.join(__dirname, './proto/exampleAPI.proto')
-const package = build(PROTO_PATH);
+const CONFIG_OBJECT = {
+  keepCase: true, // keeps everything camelCased
+  longs: Number, // transpiles the enormous `double`s for our HeavyMath into a javascript Number rather than a String
+}
+const package = build( PROTO_PATH, CONFIG_OBJECT );
 
 module.exports = package;
