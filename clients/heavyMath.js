41c01c580924f1a4b2b799fddb87ee30b5420f03
dnaganog <dnaganog@gmail.com>
2019-08-03 13:46:53 -0400

    changed logging behavior to be duplex

--- clients/heavyMath.js
+++ clients/heavyMath.js
@@ -4,8 +4,23 @@ const bidiStub = new Stub(
 	package.HeavyMath, 
 	'localhost: 2999',
 );
+let start;
+let end;
 const bidiStream = bidiStub.bidiMath();
-bidiStream.write({num: 2});
-bidiStream.on('data', ({num}) => {
-  bidiStream.write({num: num*1.1});
-})
+bidiStream.write({req: 1, res: 0});
+bidiStream.on('data', ({req, res}) => {
+	if (res === 1) {
+		start = Number(process.hrtime.bigint());
+		console.log(
+			'first response received from server',
+		)
+	}
+		bidiStream.write({req: req + 1, res: res});
+	if (res % 1000000 === 0) {
+		end = Number(process.hrtime.bigint());
+		console.log(
+			'number of responses:', res,
+			'\navg millisecond speed per response:', ((end - start) /1000000) / res,
+		)
+	}
+});
--- server/heavyMathHandlers.js
+++ server/heavyMathHandlers.js
@@ -1,19 +1,21 @@
-let count = 0;
 let start;
+let end;
 function BidiMathHandler(bidi) {
-  bidi.on('data', ({num}) => {
-    if (count === 0) start = Number(process.hrtime.bigint());
-    count += 1;
-    bidi.write({num: num*1.1});
-  })
-  bidi.on('end', () => {
-    const end = Number(process.hrtime.bigint());
+  bidi.on('data', ({req, res}) => {
+    if (req === 1) {
+      start = Number(process.hrtime.bigint());
+      console.log(
+        'first request received from client stub',
+      )
+    }
+    bidi.write({req: req, res: res + 1});
+    if (req % 1000000 === 0) {
+      end = Number(process.hrtime.bigint());
     console.log(
-      'finalNumberOfRequests:', count);
-    console.log(
-      'avg millisecond speed per request:', ((end - start) /1000000) / count
+      'number of requests:', req,
+      '\navg millisecond speed per request:', ((end - start) /1000000) / req
       );
-    count = 0;
+    }
   })
 }
 
